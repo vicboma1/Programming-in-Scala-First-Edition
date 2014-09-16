20eb4cb54a5c7caec9ac0a161cb3788bf8b1a5fd
Victor Bolinches <vicboma1@users.noreply.github.com>
2014-09-16 23:56:34 +0200

    ParameterizeArrays

---
+++ src/chapter3/ParameterizeArraysWithTypes.scala
@@ -0,0 +1,53 @@
+package chapter3
+
+/**
+ * Created by vicboma on 16/09/14.
+ */
+object ParameterizeArraysWithTypes {
+  def main(args: Array[String]) {
+    val list: Array[String] = Initialize();
+    foreach(list)
+    toStreamForeach(list)
+    forClassic(list)
+  }
+
+  private def forClassic(list: Array[String]) {
+    var init: Long = CurrentTimeMillis
+    for (i <- 0.to(2))
+      print(list.apply(i))
+    var end = CurrentTimeMillis;
+    println("Execute Time for classic: " + (end - init) + " ms")
+  }
+
+  private def toStreamForeach(list: Array[String]) {
+    var init: Long = CurrentTimeMillis
+    list.toStream.foreach(print)
+    var end = CurrentTimeMillis;
+    println("Execute Time toStream: " + (end - init) + " ms")
+  }
+
+  private def foreach(list: Array[String]) {
+    var init: Long = CurrentTimeMillis
+    list.foreach(print)
+    var end = CurrentTimeMillis;
+    println("Execute Time foreach: " + (end - init) + " ms")
+  }
+
+  private def Initialize(): Array[String] = {
+    var init: Long = CurrentTimeMillis
+
+    val list = new Array[String](3)
+    list(0) = "Hello"
+    list(1) = ","
+    list(2) = " World!"
+
+    var end = CurrentTimeMillis;
+    println("Execute Time: " + (end - init) + " ms")
+    list
+  }
+
+  private def CurrentTimeMillis: Long = {
+    var init = System.currentTimeMillis()
+    init
+  }
+}
